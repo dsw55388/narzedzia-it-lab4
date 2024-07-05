691d999b0c6765621391d6ed666f01981a43d0d7
Tomasz <dsw55388@student.dsw.edu.pl>
2024-07-05 13:47:17 +0200

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -4,9 +4,25 @@ function show_date {
     echo "Date: $(date)"
 }
 
+function create_logs {
+    local num_files=${1:-100}
+    for ((i=1; i<=num_files; i++)); do
+        filename="log${i}.txt"
+        echo -e "Filename: $filename\nScript: skrypt.sh\nDate: $(date)" > "$filename"
+    done
+    echo "$num_files log files created."
+}
+
 case $1 in
     --date)
         show_date
+        ;;
+    --logs)
+        if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+            create_logs "$2"
+        else
+            create_logs
+        fi
         ;;
     *)
         echo "Invalid option"
